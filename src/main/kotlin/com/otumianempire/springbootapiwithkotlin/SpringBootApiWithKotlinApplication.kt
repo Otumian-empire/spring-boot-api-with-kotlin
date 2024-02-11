af73dddf3f5c8725aee0290b5746521ed3f6a917
otumian <popecan1000@gmail.com>
2024-02-11 17:21:35 +0000

    Intial commit

---
+++ src/main/kotlin/com/otumianempire/springbootapiwithkotlin/SpringBootApiWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.otumianempire.springbootapiwithkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootApiWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootApiWithKotlinApplication>(*args)
+}
