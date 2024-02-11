595113221c8b4f3caf20d5014c281cd7705c2687
otumian <popecan1000@gmail.com>
2024-02-11 17:23:10 +0000

    implement a GET helloword endpoint

--- src/main/kotlin/com/otumianempire/springbootapiwithkotlin/SpringBootApiWithKotlinApplication.kt
+++ src/main/kotlin/com/otumianempire/springbootapiwithkotlin/SpringBootApiWithKotlinApplication.kt
@@ -2,9 +2,15 @@ package com.otumianempire.springbootapiwithkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
-class SpringBootApiWithKotlinApplication
+class SpringBootApiWithKotlinApplication {
+	@GetMapping
+	fun helloWorld() = "Hello world"
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootApiWithKotlinApplication>(*args)
